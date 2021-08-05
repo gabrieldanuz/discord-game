d247abe480c841d9c3081e778741c545d0d06cdc
eliasflach <elias@fortalezatec.com.br>
2021-08-05 17:27:40 -0300

    Commit 3

--- src/screens/AppointmentCreate/index.tsx
+++ src/screens/AppointmentCreate/index.tsx
@@ -1,87 +1,133 @@
-import React from 'react'
-import { BorderlessButton } from 'react-native-gesture-handler';
-import { Fontisto } from '@expo/vector-icons';
+import React, { useState } from 'react';
+import { Feather } from '@expo/vector-icons';
+import { RectButton } from 'react-native-gesture-handler';
 
-import { ImageBackground, Text, View, FlatList } from 'react-native';
+import { Text, View, ScrollView, KeyboardAvoidingView, Platform} from 'react-native';
 
 import { Background } from "../../components/background";
-import { ListHeader } from "../../components/ListHeader";
-import { ButtonIcon } from "../../components/ButtonIcons";
+import { CategorySelect } from "../../components/CategorySelect";
 import { Header } from "../../components/Header";
-import { Member } from "../../components/Member";
-import { ListDivider } from "../../components/ListDivider";
+import { GuildIcon } from "../../components/GuildIcon";
+import { SmallInput } from "../../components/SmallInput";
+import { TextArea } from "../../components/TextArea";
+import { Button } from "../../components/Button";
+import { ModalView } from "../../components/ModalView";
+import { Guilds } from "../Guilds";
+import { GuildProps } from '../../components/Guild';
+
 
 import { theme } from '../../global/styles/theme';
 import { styles } from './styles';
-import BannerImg from '../../assets/banner.png';
 
 
-export function AppointmentDetails() {
-    const members = [
-        {
-            id: '1',
-            username: 'Jonas',
-            avatar_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpWgduIfOrtiY2W_0adZ3jiYiyho9-uVOXNg&usqp=CAU',
-            status: 'online',
-        },
-        {
-            id: '2',
-            username: 'Mathias',
-            avatar_url: 'https://upload.wikimedia.org/wikipedia/commons/3/33/FC_Admira_Wacker_M%C3%B6dling_vs._FC_Red_Bull_Salzburg_%28Cup%29_2017-04-26_%28061%29.jpg',
-            status: 'ofline',
-        },
-    ]
+export function AppointmentCreate() {
+    const [category, setCategory] = useState('');
+    const [openGuildsModal, setOpenGuildsModal] = useState(false);
+    const [guild, setGuild] = useState <GuildProps> ({} as GuildProps);
+
+    function handleOpenGuilds() {
+        setOpenGuildsModal(true);
+    }
+
+    function handleGuildSelect(guildSelect: GuildProps) {
+        setGuild(guildSelect)
+        setOpenGuildsModal(false);
+    }
+
     return (
-        <Background>
-            <Header
-                title="Detalhes"
-                action={
-                    <BorderlessButton>
-                        <Fontisto
-                            name="share"
-                            size={24}
-                            color={theme.colors.primary}
-                        />
-                    </BorderlessButton>
-                }
-            />
+        <KeyboardAvoidingView style={styles.container}
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        >
+            <ScrollView>
+                <Background>
+                    <Header
+                        title="Agendar partida"
+                    />
 
-            <ImageBackground
-                source={BannerImg}
-                style={styles.banner}
-            >
-                <View style={styles.bannerContent}>
-                    <Text style={styles.title}>
-                        Lendários
+                    <Text style={[styles.label, { marginLeft: 24, marginTop: 36, marginBottom: 18 }]}>
+                        Categoria
                     </Text>
+                    <CategorySelect
+                        hasCheckBox
+                        setCategory={setCategory}
+                        categorySelected={category}
+                    />
 
-                    <Text style={styles.subtitle}>
-                        É hoje que vamos chegar ao challenger sem perder uma partida da md10
-                    </Text>
-                </View>
-            </ImageBackground>
+                    <View style={styles.form}>
+                        <RectButton onPress={handleOpenGuilds}>
 
-            <ListHeader
-                title="Jogadores"
-                subtitle="Total 3" />
+                            <View style={styles.select}>
+                                {
+                                 guild.icon ? <GuildIcon /> : <View style={styles.image} />
+                                }
+                                <View style={styles.selectBody}>
+                                    <Text style={styles.label}>
+                                    {guild.name ? guild.name :  'Selecione um servidor' }
+                                    </Text>
+                                </View>
+                                <Feather
+                                    name="chevron-right"
+                                    color={theme.colors.heading}
+                                    size={18}
+                                />
+                            </View>
+                        </RectButton>
 
-            <FlatList
-                data={members}
-                keyExtractor={item => item.id}
-                renderItem={({ item }) => (
-                    <Member data={item} />
-                )}
-                ItemSeparatorComponent={() => <ListDivider />}
-                style = {styles.members}
-            />
+                        <View style={styles.field}>
+                            <View>
+                                <Text style={styles.label}>
+                                    Dia e Mês
+                                </Text>
+                                <View style={styles.column}>
+                                    <SmallInput maxLength={2} />
+                                    <Text style={styles.divider}>
+                                        /
+                                    </Text>
+                                    <SmallInput maxLength={2} />
+                                </View>
+                            </View>
 
-        <View style={styles.footer}>
-            <ButtonIcon title="Entrar na partida"
+                            <View>
+                                <Text style={styles.label}>
+                                    Hora e Minuto
+                                </Text>
+                                <View style={styles.column}>
+                                    <SmallInput maxLength={2} />
+                                    <Text style={styles.divider}>
+                                        :
+                                    </Text>
+                                    <SmallInput maxLength={2} />
+                                </View>
+                            </View>
+                        </View>
 
-            />
 
-    </View>
-        </Background>
-    )
+                        <View style={[styles.field, { marginBottom: 12 }]}>
+                            <Text style={styles.label}>
+                                Descrição
+                            </Text>
+
+                            <Text style={styles.caracteresLimit}>
+                                Max 100 caracteres
+                            </Text>
+                        </View>
+                        <TextArea
+                            multiline
+                            maxLength={100}
+                            numberOfLines={5}
+                            autoCorrect={false} />
+
+                        <View style={styles.footer}>
+                            <Button title= "Agendar"/>
+                        </View>
+                    </View>
+                </Background>
+            </ScrollView>
+
+            <ModalView visible={openGuildsModal}>
+                <Guilds handleGuildSelect={handleGuildSelect}/>
+            </ModalView>
+        </KeyboardAvoidingView>
+    );
 }
 
